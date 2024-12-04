e2a7959b8d14e1ac4ac42ec9cf8d478cdf11fa33
shubham122005 <shubhamsaim999@hgmail.com>
2024-12-04 12:29:16 +0530

    introduction to react

--- react work/reactvitapp/src/App.jsx
+++ react work/reactvitapp/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Student from './Student'
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <div>
+        <h2 style={{color:'red'}}> Hello React JS</h2>
+        <div>
+          <Student />
+        </div>
+      </div>
+    </div>
   )
 }
 
